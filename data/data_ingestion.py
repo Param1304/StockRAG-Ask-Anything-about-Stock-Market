e1a31ca55dafcbdf992be241091f1345b6a8ff32
Param Parekh <129577837+Param1304@users.noreply.github.com>
2025-01-10 15:25:14 +0530

    Update data_ingestion.py

--- data/data_ingestion.py
+++ data/data_ingestion.py
@@ -1,1 +1,11 @@
-
+dataset_name = "yymYYM/stock_trading_QA"
+def data_ingestion(dataset_name):
+    dataset = load_dataset(dataset_name)
+    RAW_KNOWLEDGE_BASE = [
+    LangchainDocument(
+        page_content=f"{ex['question']}\n\n{ex['answer']}", 
+        metadata={"source": "stock_trading_QA"}
+    ) 
+    for ex in tqdm(dataset["train"]) 
+    ]
+    return RAW_KNOWLEDGE_BASE
